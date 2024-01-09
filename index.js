f2f8d4ccd2edef2d669973ff7b5dca21251ef6e2
Phathdt <phathdt379@gmail.com>
2024-01-09 12:41:25 +0700

    init

--- index.js
+++ index.js
@@ -6,19 +6,19 @@ import fs from 'fs'
 const filePath = './erc20.json'
 const ERC20ABI = JSON.parse(fs.readFileSync(filePath, 'utf-8'))
 
-const app = express()
-app.use(cors())
-app.use(json())
+// const app = express()
+// app.use(cors())
+// app.use(json())
 
-app.get('/', (req, res) => {
-  const { param1 } = req.query
+// app.get('/', (req, res) => {
+//   const { param1 } = req.query
 
-  res.send('Hello World!<br>Param1 = ' + param1)
-})
+//   res.send('Hello World!<br>Param1 = ' + param1)
+// })
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000!')
-})
+// app.listen(3000, () => {
+//   console.log('Listening on port 3000!')
+// })
 
 async function getEthBalance(address) {
   // Connect to the Ethereum network using a provider
@@ -28,25 +28,25 @@ async function getEthBalance(address) {
 
   try {
     // Get the balance of the specified address
-    // const balanceWei = await provider.getBalance(address)
+    const balanceWei = await provider.getBalance(address)
 
     // Convert balance from Wei to Ether
-    // const balanceEther = ethers.formatEther(balanceWei)
+    const balanceEther = ethers.formatEther(balanceWei)
 
-    // console.log(`Balance of ${address}: ${balanceEther} ETH`)
+    console.log(`Balance of ${address}: ${balanceEther} ETH`)
 
     const contact = new ethers.Contract(
       '0x7d773Af3d29743442Dc991c168C434D99836A723',
       ERC20ABI,
       provider
     )
-    const balanceWei = await contact.balanceOf(address)
-    console.log(`Balance of ${address}: ${balanceWei} USDT`)
+    const balance = await contact.balanceOf(address)
+    console.log(`Balance of ${address}: ${balance} USDT`)
 
-    const balanceEther = ethers.formatEther(balanceWei)
-    console.log(`Balance of ${address}: ${balanceEther} USDT`)
+    const formatBalance = ethers.formatEther(balance)
+    console.log(`Balance of ${address}: ${formatBalance} USDT`)
 
-    return balanceEther
+    return formatBalance
   } catch (error) {
     console.error('Error getting balance:', error)
     throw error
